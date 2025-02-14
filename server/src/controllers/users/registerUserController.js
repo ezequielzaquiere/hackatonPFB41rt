bca2102c77d28b047ae8ac79c36bf7f5ae71a137
Cristina Filgueira <crisfilgueiracano@gmail.com>
2025-02-14 21:05:48 +0100

    Terminar registerUserController

--- server/src/controllers/users/registerUserController.js
+++ server/src/controllers/users/registerUserController.js
@@ -1,8 +1,27 @@
+// Importamos los modelos necesarios.
+import insertUserModel from '../../models/users/insertUserModel.js';
+
+// Importamos la función que genera un error.
+import generateErrorUtil from '../../utils/generateErrorUtil.js';
+
+// Función controladora que registra un nuevo usuario.
 const registerUserController = async (req, res, next) => {
     try {
+        // Obtenemos los datos necesarios del body.
+        const { username, email, password } = req.body;
+
+        // Lanzamos un error si falta algún campo.
+        if (!username || !email || !password) {
+            generateErrorUtil('Faltan campos', 400);
+        }
+
+        // Insertamos el usuario.
+        await insertUserModel(username, email, password);
+
         res.status(201).send({
             status: 'ok',
-            message: 'Usuario creado',
+            message:
+                'Usuario creado. Por favor, activa tu usuario en el email de verificación que recibirás en tu correo',
         });
     } catch (err) {
         next(err);
