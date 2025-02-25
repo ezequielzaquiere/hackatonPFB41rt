45df6c6e81a8cc0aa5c96a912670a60d12be8150
Ezequiel Zaquiere <ezequielzaquiere@gmail.com>
2025-02-25 19:01:57 +0100

    feat: structure HomePage + events slider + FAQ function

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,10 @@
+import HomePage from './pages/HomePage';
+
 function App() {
     return (
         <>
             <h1 className="text-red-500">Vite + React</h1>
+            <HomePage />
         </>
     );
 }
