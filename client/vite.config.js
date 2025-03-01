ed78b7234ee09e464788ebc6c9ab3a4ba8e1a115
Ezequiel Zaquiere <ezequielzaquiere@gmail.com>
2025-03-01 19:19:10 +0100

    feat: UseRecoveryPassCode (ahora desde el mail, accedemos al link para entrar en la pagina de nueva contraseña)

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,5 +7,15 @@ export default defineConfig({
     plugins: [tailwindcss(), react()],
     server: {
         port: 8000,
+        historyApiFallback: true, // Redirige rutas desconocidas en desarrollo
+    },
+    build: {
+        rollupOptions: {
+            output: {
+                entryFileNames: `[name].js`,
+                chunkFileNames: `[name].js`,
+                assetFileNames: `[name].[ext]`,
+            },
+        },
     },
 });
