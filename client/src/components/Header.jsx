a741d359182979361ee30a7ada3d9c2e6a90f279
Liss Recalde Zurita <elizabeth.rec.zur@gmail.com>
2025-03-05 20:52:37 +0100

    styling header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,18 +8,12 @@ const { VITE_API_URL } = import.meta.env;
 const Header = () => {
     const { authUser, authLogoutState } = useContext(AuthContext);
     return (
-        <header>
-            <h1 className="text-red-500">
-                {' '}
-                <Link to="/">HACKVERSE</Link>
-            </h1>
+        <header className="bg-[#9A4EAE] text-[#191919] p-6 lg:px-12 lg:py-8">
             {
                 // Si el usuario está autenticado, mostramos su información.
                 authUser && (
-                    <div className="user-info">
-                        <p>
-                            @{authUser.username} ({authUser.role})
-                        </p>
+                    <div className="absolute top-2 right-2 flex items-center space-x-2">
+                        <p className="text-white">@{authUser.username}</p>
                         <img
                             src={
                                 authUser.avatar
@@ -27,42 +21,67 @@ const Header = () => {
                                     : defaultAvatar
                             }
                             alt={authUser.username}
+                            className="h-12 w-12 rounded-full border-2 border-white shadow-md"
                         />
                     </div>
                 )
             }
 
-            <nav>
+            <nav className="flex flex-col lg:flex-row justify-center space-y-4 lg:space-y-0 lg:space-x-6">
                 <ul>
+                    <li>
+                        <Link
+                            to="/"
+                            className="text-white hover:text-gray-300 transition"
+                        >
+                            Página principal
+                        </Link>
+                    </li>
                     {!authUser ? (
                         <>
                             <li>
-                                <Link to="/register">Registro</Link>
+                                <Link
+                                    to="/register"
+                                    className="text-white hover:text-gray-300 transition"
+                                >
+                                    Registro
+                                </Link>
                             </li>
                             <li>
-                                <Link to="/login">Iniciar Sesión</Link>
+                                <Link
+                                    to="/login"
+                                    className="text-white hover:text-gray-300 transition"
+                                >
+                                    Iniciar Sesión
+                                </Link>
                             </li>
                         </>
                     ) : (
                         <>
                             {authUser.role === 'admin' && (
                                 <li>
-                                    <Link to="/hackathon/new">
+                                    <Link
+                                        to="/hackathon/new"
+                                        className="text-white hover:text-gray-300 transition"
+                                    >
                                         Nuevo Hackathon
                                     </Link>
                                 </li>
                             )}
 
                             <li>
-                                <Link to="/users/profile">Mi perfil</Link>
-                            </li>
-                            <li>
-                                <Link to="/users/profile/public">
-                                    Perfil público
+                                <Link
+                                    to="/users/profile"
+                                    className="text-white hover:text-gray-300 transition"
+                                >
+                                    Mi perfil
                                 </Link>
                             </li>
                             <li>
-                                <button onClick={() => authLogoutState()}>
+                                <button
+                                    onClick={() => authLogoutState()}
+                                    className="text-white hover:text-gray-300 transition"
+                                >
                                     {' '}
                                     Cerrar Sesión
                                 </button>
